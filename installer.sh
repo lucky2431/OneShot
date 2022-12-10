4970820796181414b57142a8b80de2056955a8b9
astrofic <astrofic.guru@gmail.com>
2022-12-11 03:49:33 +0500

    fix

--- installer.sh
+++ installer.sh
@@ -5,9 +5,9 @@ pkg install -y git tsu python wpa-supplicant pixiewps iw
 
 git clone --depth 1 https://github.com/Lunatik-cyber/OneShot OneShot
 
-chmod +x OneShot/oneshot.py
+chmod +x /data/data/com.termux/files/home/OneShot/oneshot.py
 
-echo "sudo python3 OneShot/oneshot.py -i wlan0 -K" > /data/data/com.termux/files/usr/bin/hack
+echo "cd /data/data/com.termux/files/home/OneShot; sudo python3 oneshot.py -i wlan0 -K" > /data/data/com.termux/files/usr/bin/hack
 chmod +x /data/data/com.termux/files/usr/bin/hack
 clear
 printf "#########################################################\n#  All done! Now you can run OneShot with\n#   sudo python3 OneShot/oneshot.py -i wlan0 -K or hack\n#\n#  To update, run\n#   (cd OneShot && git pull)\n#########################################################\n"
