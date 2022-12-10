56d68243e2fb036c5531d6b46edbb17263d4ad87
astrofic <astrofic.guru@gmail.com>
2022-12-11 03:46:58 +0500

    fix

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ git clone --depth 1 https://github.com/Lunatik-cyber/OneShot OneShot
 
 chmod +x OneShot/oneshot.py
 
-echo "cd OneShot; sudo python3 OneShot/oneshot.py -i wlan0 -K" > /data/data/com.termux/files/usr/bin/hack
+echo "sudo python3 OneShot/oneshot.py -i wlan0 -K" > /data/data/com.termux/files/usr/bin/hack
 chmod +x /data/data/com.termux/files/usr/bin/hack
 clear
 printf "#########################################################\n#  All done! Now you can run OneShot with\n#   sudo python3 OneShot/oneshot.py -i wlan0 -K or hack\n#\n#  To update, run\n#   (cd OneShot && git pull)\n#########################################################\n"
