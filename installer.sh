996af5c6991f291c3908c5a697817d468aaa2299
lucky2431 <lucky2431@gmail.com>
2023-08-25 09:21:38 +0300

    Update installer.sh
    with --iface-down starting better (lower chance to bad start), without sudo cat can't show/read passwords (stored file)

--- installer.sh
+++ installer.sh
@@ -7,8 +7,8 @@ git clone --depth 1 https://github.com/Lunatik-cyber/OneShot OneShot
 
 chmod +x /data/data/com.termux/files/home/OneShot/oneshot.py
 chmod +x /data/data/com.termux/files/home/OneShot/wifi_connect.sh
-echo "cd /data/data/com.termux/files/home/OneShot; sudo python3 oneshot.py -i wlan0 -K -w" > /data/data/com.termux/files/usr/bin/hack
-echo "clear; cat /data/data/com.termux/files/home/OneShot/reports/stored.txt" > /data/data/com.termux/files/usr/bin/hacked
+echo "cd /data/data/com.termux/files/home/OneShot; sudo python3 oneshot.py --iface-down -i wlan0 -K -w" > /data/data/com.termux/files/usr/bin/hack
+echo "clear; sudo cat /data/data/com.termux/files/home/OneShot/reports/stored.txt" > /data/data/com.termux/files/usr/bin/hacked
 chmod +x /data/data/com.termux/files/usr/bin/hack
 chmod +x /data/data/com.termux/files/usr/bin/hacked
 clear
